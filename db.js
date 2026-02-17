17d7e8d2e0077fa37be2ef75af5c509ae1b67503
Kuldeepbishnoi2005 <kuldeepbeniwal2005@gmail.com>
2026-02-18 03:21:19 +0530

    update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'mysql.railway.app',
+  host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'kRCOoUEexQoCVionwmTpWtyeCFSbxrkB',
   database: process.env.DB_NAME || 'railway',
