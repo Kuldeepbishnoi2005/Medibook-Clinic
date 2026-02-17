1ef509d8d740d7c275bf4c04474a727ca6fbcc8c
Kuldeepbishnoi2005 <kuldeepbeniwal2005@gmail.com>
2026-02-18 02:21:51 +0530

    update db.js

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'yamabiko.proxy.rlwy.net',
+  host: process.env.DB_HOST || 'mysql.railaway.app',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'kRCOoUEexQoCVionwmTpWtyeCFSbxrkB ',
-  database: process.env.DB_NAME || 'Railway MediBook',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
